2a9bba7211548bf3022e950367516f99d13b1d09
Lera <it.valeriia22@gmail.com>
2022-09-06 18:22:11 +0300

    index.js: обернула <App /> <Provider>, создала редюсер

---
+++ src/components/redux-store/reducer.js
@@ -0,0 +1,12 @@
+//пример из урока react redux 46:30
+
+const initialState = 2;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type){
+  //  условия
+  }
+  return state;
+};
+
+export default reducer;
