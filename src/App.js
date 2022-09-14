c2af1e6e2ee00d49fbe5a972beaee92575d8d314
AlinaVytiuk <alina.vityuk69@gmail.com>
2022-09-14 20:22:03 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import './styles/app.scss';
 import Header from "./components/Header";
-import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Footer from "./components/Footer";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className='container'>
-                <Header/>
-                <main>
-                    <AppRouter />
-                </main>
-                <Footer />
-            </div>
-        </BrowserRouter>
+        <div className='container'>
+            <Header/>
+            <main>
+                <AppRouter />
+            </main>
+            <Footer />
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import reducer from "./redux-store/reducer";
+import {BrowserRouter} from "react-router-dom";
 
 const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
