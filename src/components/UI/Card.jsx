381e31dcc335d378f9d2c0da5138ad6849deaf26
Valeriy <balorok5@gmail.com>
2022-10-06 21:01:57 +0300

    imgs added

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -4,12 +4,18 @@ import Item from '../../images/card-item.jpg';
 
 const Card = (props) => {
 
-    const {name, image, id, price} = props;
+    const {name, mainimage, id, price} = props;
+
+    console.log(mainimage);
+
+    const imgPath = '../../images/cards-img/';
+
+    const imageSource = imgPath + mainimage;
 
     return (
         <div className="cards__card flex-column">
             <div className="cards__image-wrapper">
-                <img src={Item}/>
+                <img src={require( `${imageSource}` )}/>
             </div>
             <h2 className="cards__headline">{name}</h2>
             <p className="cards__price">{price}</p>
