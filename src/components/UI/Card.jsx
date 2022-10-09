527f6f6f255f3271fae4c25034ba02096ccd991e
Lera <it.valeriia22@gmail.com>
2022-10-09 23:47:04 +0300

    img in cards

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -4,17 +4,17 @@ import Item from '../../images/card-item.jpg';
 
 const Card = (props) => {
 
-    const {name, image, id, price} = props;
+  const {name, mainImage, id, price} = props;
 
-    return (
-        <div className="cards__card flex-column">
-            <div className="cards__image-wrapper">
-                <img src={Item}/>
-            </div>
-            <h2 className="cards__headline">{name}</h2>
-            <p className="cards__price">{price}</p>
-        </div>
-    );
+  return (
+    <div className="cards__card flex-column">
+      <div className="cards__image-wrapper">
+        <img src={mainImage}/>
+      </div>
+      <h2 className="cards__headline">{name}</h2>
+      <p className="cards__price">{price}</p>
+    </div>
+  );
 };
 
 export default Card;
