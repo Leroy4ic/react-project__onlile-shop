48d7430c1e589132efe949f006c66af92116ed24
Valeriy <balorok5@gmail.com>
2022-10-12 20:00:04 +0300

    Have added our json base

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -4,18 +4,16 @@ import Item from '../../images/card-item.jpg';
 
 const Card = (props) => {
 
-    const {name, mainimage, id, price} = props;
+    const {name, mainImage, id, price} = props;
 
-    console.log(mainimage);
+    console.log(mainImage);
 
     const imgPath = '../../images/cards-img/';
-
-    const imageSource = imgPath + mainimage;
 
     return (
         <div className="cards__card flex-column">
             <div className="cards__image-wrapper">
-                <img src={require( `${imageSource}` )}/>
+                <img src={require( `../../images/cards-img/${mainImage}` )}/>
             </div>
             <h2 className="cards__headline">{name}</h2>
             <p className="cards__price">{price}</p>
