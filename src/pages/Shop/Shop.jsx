1ab25596c7b313806b36dcdd404247b2f5eca31d
Valeriy <balorok5@gmail.com>
2022-10-14 00:10:59 +0300

    Attention! Not working!

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from "../../components/UI/Card";
 import Caption from "../../components/UI/Caption";
 import {Link} from "react-router-dom";
@@ -6,17 +6,43 @@ import useCatalogData from "../../custom-hooks/useCatalogData";
 
 const Shop = () => {
 
-    const coat = useCatalogData('coat');
-    console.log(coat?.data);
+    // const coat = useCatalogData('coat');
+    // const coatData = coat?.data;
+    //
+    // const cardigan = useCatalogData('cardigan');
+    // const cardiganData = cardigan?.data;
+    //
+    // const sweatshirt = useCatalogData('sweatshirt');
+    // const sweatshirtData = sweatshirt?.data;
+    //
+    // const tolstovki = useCatalogData('tolstovki');
+    // const tolstovkiData = tolstovki?.data;
 
-    const coatData = coat?.data;
-    console.log(coatData);
+    const womazing = useCatalogData('womazing');
+
+    const [dataBase, setDataBase] = useState('');
+
+    useEffect( () => {
+        setDataBase(womazing);
+    }, [womazing]);
+
+
+
+    console.log(womazing);
 
     const categories = ['Все', 'Пальто', 'Свитшоты', 'Кардиганы', 'Толстовки'];
     const [status, setStatus] = useState('all');
 
-    const hand = () => {
+    // const allData = [coatData, cardiganData, sweatshirtData, tolstovkiData];
 
+    const [catalog, setCatalog] = useState(null);
+
+    const hand = (e) => {
+        // if(e.target.innerText === 'Все') {
+        //     // console.log('hello');
+        //     setCatalog(allData);
+        //     console.log(catalog);
+        // };
     }
 
     return (
@@ -31,9 +57,28 @@ const Shop = () => {
             </ul>
             <p>Показано: 9 из 12 товаров</p>
             <div className="cards flex">
-                {coatData?.map(item => {
-                    return <Card key={item.id} {...item}/>
+
+                {Object.keys(dataBase)?.forEach(data => {
+                    data?.map(item => {
+                        return <Card key={item.id} {...item}/>
+                    })
                 })}
+
+                {/*{coatData?.map(item => {*/}
+                {/*    return <Card key={item.id} {...item}/>*/}
+                {/*})}*/}
+
+                {/*{cardiganData?.map(item => {*/}
+                {/*    return <Card key={item.id} {...item}/>*/}
+                {/*})}*/}
+
+                {/*{sweatshirtData?.map(item => {*/}
+                {/*    return <Card key={item.id} {...item}/>*/}
+                {/*})}*/}
+
+                {/*{tolstovkiData?.map(item => {*/}
+                {/*    return <Card key={item.id} {...item}/>*/}
+                {/*})}*/}
 
             </div>
             <div className="pagination flex">
