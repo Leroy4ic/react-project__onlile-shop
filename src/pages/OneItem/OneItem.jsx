04d8b224338437aeddb74c04f9e7fb5666533e79
Lera <it.valeriia22@gmail.com>
2022-10-14 22:29:38 +0300

    no img in cards, color div on the home page is not too wide page "About" is finished

---
+++ src/pages/OneItem/OneItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Caption from '../../components/UI/Caption';
+
+const OneItem = () => {
+  return (
+    <div>
+      <Caption caption={}/>
+
+    </div>
+  );
+};
+
+export default OneItem;
