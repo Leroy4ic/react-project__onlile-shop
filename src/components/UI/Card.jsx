7d59c52cb8c7f19a761ee8f6834002f640a96d29
Lera <it.valeriia22@gmail.com>
2022-10-14 20:21:00 +0300

    no img in cards, color div on the home page is not too wide

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import Item from '../../images/card-item.jpg';
+import Item from '../../images/cards-img/card-item.jpg';
 
 
 const Card = (props) => {
 
   const {name, mainImage, id, price} = props;
 
+  // console.log(mainImage);
+
   return (
     <div className="cards__card flex-column">
       <div className="cards__image-wrapper">
-        <img src={mainImage}/>
+        {/*<img src={require(`../../images/cards-img/${mainImage}`)}/>*/}
+        <img src={mainImage ? require(`../../images/cards-img/${mainImage}`) : Item}/>
       </div>
       <h2 className="cards__headline">{name}</h2>
       <p className="cards__price">{price}</p>
